030f5a437d440b50007fc1d568401b90cc09a504
Yeonu Choi <foydh.o53@gmail.com>
2023-09-10 00:06:05 +0900

    update

--- app.py
+++ app.py
@@ -6,9 +6,18 @@ import pandas as pd
 
 def main():
     
-    st.markdown("# Hello World")
-    st.write(np.__version__)
-    st.write(pd.__version__)
+    st.markdown("Hello World")
+    menu = ["Home", "탐색적 자료 분석", "머신러닝", "About"]
+    choice = st.sidebar.selectbox("메뉴", menu)
+
+    if choice == "Home":
+        st.subheader("Home")
+    elif choice == "탐색적 자료 분석":
+        run_eda_app()
+    elif choice == "머신러닝":
+        run_ml_app()
+    else:
+        st.subheader("About")
 
 
 if __name__ == "__main__":
