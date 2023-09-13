1a75716a6a5106f18de216312a8a316da1c90e01
Yeonu Choi <foydh.o53@gmail.com>
2023-09-13 16:52:31 +0900

    update

--- ml_app.py
+++ ml_app.py
@@ -20,10 +20,43 @@ def run_ml_app():
         sample_list = [sepal_length, sepal_width, petal_length, petal_width]
         st.write(sample_list)
 
-    with cos2:
+    with col2:
         st.subheader("모델 결과를 확인해주세요!")
 
         # 모델 불러오기
         model_file = "models/lgr_model_iris230331.pkl"
         model = joblib.load(open(os.path.join(model_file), "rb"))
         st.write(model)
+
+        # 배열로 만듬
+        one_sample = np.array(sample_list).reshape(1, -1)
+        st.write(one_sample)
+        st.write(one_sample.shape)
+
+        # 범주 예측
+        prediction = model.predict(one_sample)
+        st.write(prediction)
+
+        # 확률값 예측
+        pred_prob = model.predict_proba(one_sample)
+        st.write(pred_prob)
+
+
+        if prediction == 0:
+            st.success("Setosa 종")
+            pred_proba_scores = {
+                "1일 확률": pred_prob[0][0] * 100, 
+                "0일 확률": pred_prob[0][1] * 100, 
+            }
+            st.write(pred_proba_scores)
+            st.image('https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/Irissetosa1.jpg/220px-Irissetosa1.jpg')
+        elif prediction == 1:
+            st.success("00 종")
+            pred_proba_scores = {
+                "1일 확률": pred_prob[0][0] * 100, 
+                "0일 확률": pred_prob[0][1] * 100, 
+            }
+            st.write(pred_proba_scores)
+            st.image('https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/Blue_Flag%2C_Ottawa.jpg/220px-Blue_Flag%2C_Ottawa.jpg')
+        else:
+            st.warning("아이돈노!")
