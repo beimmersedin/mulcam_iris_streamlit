3c62846ceaf55fe2ccb5fa440fd4b40b966c4700
Yeonu Choi <foydh.o53@gmail.com>
2023-09-13 12:26:15 +0900

    update

--- eda_app.py
+++ eda_app.py
@@ -12,5 +12,12 @@ def run_eda_app():
     st.subheader("탐색적 자료 분석 페이지")
     st.subheader("잘 진행 중임 .. !")
 
+    iris_df = pd.read_csv("data/iris.csv")
+    
+    st.dataframe(iris_df)
+
+
+
+
 if __name__ == "__main__":
     main()
